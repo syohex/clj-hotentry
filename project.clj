4338de5cdff1bc5614c8a5be1b3a7911b543ec60
Syohei YOSHIDA <syohex@gmail.com>
2015-04-01 16:35:42 +0900

    init repos

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject hotentry "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "hotentry viewer in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :main hotentry.core
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [adamwynne/feedparser-clj "0.5.2"]])
--- src/hotentry/core.clj
+++ src/hotentry/core.clj
@@ -1,6 +1,33 @@
-(ns hotentry.core)
+(ns hotentry.core
+  (:require [clojure.tools.cli :as cli]
+            [feedparser-clj.core :as feedparser]) )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private options
+  [["-t" "--threshold NUM" "threshold of bookmarks"
+    :default 3]
+   ["-l" "--limit" "limit of printing entries"
+    :default 10]
+   ["-h" "--help"]])
+
+(defn- hotentry-url [keyword threshould]
+  (format "http://b.hatena.ne.jp/search/tag?q=%s&users=%d&mode=rss"
+          keyword threshould))
+
+(defn- parse-rss [url]
+  (let [feed (feedparser/parse-feed url)]
+    (map #(select-keys % [:title :link]) (:entries feed))))
+
+(defn- format-entries [entries]
+  (map-indexed (fn [i e]
+                 (format "%2d: %s"
+                         (inc i) (:title e)))
+               entries))
+
+(defn -main [& args]
+  (let [parsed (cli/parse-opts args options)
+        opt (:options parsed)
+        key (first (:arguments parsed))
+        url (hotentry-url key (:threshold opt))
+        entries (parse-rss url)]
+    (doseq [entry (take (:limit opt) (format-entries entries))]
+      (println entry))))
